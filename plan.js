7185553e041f795d1d76eaa09e9db674af0da48b
BigC <>
2016-06-07 19:58:16 +0800

    Fixed a downloading bugs.

--- plan.js
+++ plan.js
@@ -7,9 +7,9 @@ var PlanDownloader = (function() {
     
     var i;
     var theSnippedURL = location.href.match(/([0-9]|[a-f]){32}/g);
-     if(window.location.href.indexOf("inspectionPlan") > -1)
+     if(window.location.href.indexOf("inspectionPlan?imageId") > -1)
 		{window.open('https://bravo.bd.gov.hk/getInspectionPlanImage?imageId='+theSnippedURL);
-	    setTimeout(window.close(),3000);}
+	    setTimeout(window.close(),5000);}
 			else
 				{if(window.location.href.indexOf("inspectionDocument") > -1)
 				{feed = confirm("Download Report?");
@@ -20,6 +20,7 @@ var PlanDownloader = (function() {
 						for(i = 1; i <= parseInt(x); i++)
 						{window.open('https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo='+i);
 						setTimeout(window.close(),5000);}
+						confirm("Download"+ x + "Pages Completed, Please Wait Until All Tab CLosed.");
 	      }}
 	      else
 	      {}
