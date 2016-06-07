2734af663372dc60ba6dc6386d597a340ede25b9
BigC <>
2016-06-07 20:20:53 +0800

    v6.3 , stuck when pages over 100, need to modify later.

--- plan.js
+++ plan.js
@@ -5,22 +5,22 @@
 
 var PlanDownloader = (function() {
     
-    var i;
     var theSnippedURL = location.href.match(/([0-9]|[a-f]){32}/g);
      if(window.location.href.indexOf("inspectionPlan?imageId") > -1)
 		{window.open('https://bravo.bd.gov.hk/getInspectionPlanImage?imageId='+theSnippedURL);
 	    setTimeout(window.close(),5000);}
 			else
 				{if(window.location.href.indexOf("inspectionDocument") > -1)
-				{feed = confirm("Download Report?");
+				{feed = confirm("Download Report? 50~100 pages are limited");
 					if( feed === false)
 					{prompt("URL:",'https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo=*');}
 					else
-						{var x = prompt("How many pages?","Please input numbers");
-						for(i = 1; i <= parseInt(x); i++)
-						{window.open('https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo='+i);
+						{var a = prompt("Start From Which Page","Please input a number");
+						 var b = prompt("Finish in Which Page","Please input a number");
+						for(a > 0; parseInt(a)  <= parseInt(b); a++)
+						{window.open('https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo='+a);
 						setTimeout(window.close(),5000);}
-						confirm("Download"+ x + "Pages Completed, Please Wait Until All Tab CLosed.");
+						confirm("Download Reports Completed, Please Wait Until All Tab CLosed.");
 	      }}
 	      else
 	      {}
