247fe7f84881061dc53a76fcb60a629e198e86c6
Kedros Ng <kedrosng@gmail.com>
2016-06-07 17:01:08 +0800

    Updated Icon and Review Code
    v6.0

--- plan.js
+++ plan.js
@@ -8,18 +8,18 @@ var PlanDownloader = (function() {
     var i;
     var theSnippedURL = location.href.match(/([0-9]|[a-f]){32}/g);
      if(window.location.href.indexOf("inspectionPlan") > -1)
-      {window.open('https://bravo.bd.gov.hk/getInspectionPlanImage?imageId='+theSnippedURL);
+		{window.open('https://bravo.bd.gov.hk/getInspectionPlanImage?imageId='+theSnippedURL);
 	    setTimeout(window.close(),3000);}
-	   else
-	    {if(window.location.href.indexOf("inspectionDocument") >-1)
-	    {feed = confirm("Download Report?");
-	    if( feed === false)
-	    {prompt("URL:",'https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo=*');}
-	    else
-	    {var x = prompt("How many pages?","Please input numbers");
-	    for(i = 1; i <= parseInt(x); i++)
-	    {window.open('https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo='+i);
-	      setTimeout(window.close(),5000);}
+			else
+				{if(window.location.href.indexOf("inspectionDocument") > -1)
+				{feed = confirm("Download Report?");
+					if( feed === false)
+					{prompt("URL:",'https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo=*');}
+					else
+						{var x = prompt("How many pages?","Please input numbers");
+						for(i = 1; i <= parseInt(x); i++)
+						{window.open('https://bravo.bd.gov.hk/getInspectionDocumentImage?imageId='+theSnippedURL+'&pageNo='+i);
+						setTimeout(window.close(),5000);}
 	      }}
 	      else
 	      {}
